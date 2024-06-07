dc0e5c6fe1081806857f5acc694776b498a34474
Shaq Reid-Robinson <shaqreidrobinson@gmail.com>
2024-06-07 15:50:22 +0100

    Start of automation on bank site

---
+++ dummybanksite_auto/dummybanksite_auto/Class1.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace dummybanksite_auto
+{
+    public class navChecks
+    {
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void reachSite()
+        {
+            using ChromeDriver driver = new ChromeDriver();
+            {
+                driver.Navigate().GoToUrl("https://parabank.parasoft.com/parabank/index.htm");
+                Assert.Equal("https://parabank.parasoft.com/parabank/index.htm", driver.Url);
+            }
+
+        }
+        
+    }
+}
