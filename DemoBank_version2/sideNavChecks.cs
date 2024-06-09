a29a4a608a1c4f5f55e01c9a276dc07c5585c09b
Shaq Reid-Robinson <shaqreidrobinson@gmail.com>
2024-06-09 22:05:25 +0100

    Figured out cookie value in URL issue Simply just clicked the nav element again and it cleared itself Spent way too long trying to retrieve cookie values via the driver but it kept referencing a null value. God knows why

--- DemoBank_version2/sideNavChecks.cs
+++ DemoBank_version2/sideNavChecks.cs
@@ -17,11 +17,11 @@ namespace DemoBank_version2
         }
 
         [Fact]
-        [Trait("Category", "Smoke")]
+        [Trait("Category", "Navigation")]
         public void aboutPage()
         {
             ChromeOptions options = new ChromeOptions();
-            options.AddArguments("start-maximized");
+            options.AddArguments("start-maximized", "--incognito");
             using IWebDriver driver = new ChromeDriver(options);
             {
                 driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
@@ -30,10 +30,15 @@ namespace DemoBank_version2
 
                 IWebElement aboutLink = driver.FindElement(By.XPath("//*[@id=\"headerPanel\"]/ul[1]/li[2]/a"));
                 aboutLink.Click();
-                Thread.Sleep(3000);
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+
+                IWebElement aboutLinkAgain = driver.FindElement(By.XPath("//*[@id=\"headerPanel\"]/ul[1]/li[2]/a"));
+                aboutLinkAgain.Click(); 
+                //driver.Manage().Cookies.AddCookie(new Cookie("Test", "123"));
+                //var sessionID = driver.Manage().Cookies.GetCookieNamed("jsessionid").Value;
                 // Fail Issue 1: URL upon page change via nav link adds session ID to the URL.
                 // Cannot seem replicate this via manual checks. 
-                Assert.Equal("https://parabank.parasoft.com/parabank/about.htm;jsessionid=ECEF4BE1F34392CA6DD91", driver.Url);
+                Assert.Equal("https://parabank.parasoft.com/parabank/about.htm", driver.Url);
             }
 
         }
