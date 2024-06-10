2366803e27bd90d9e81c886812452bc52a0ca071
Shaq Reid-Robinson <shaqreidrobinson@gmail.com>
2024-06-10 10:14:22 +0100

    fixed the tests. All passed

--- DemoBank_version2/sideNavChecks.cs
+++ DemoBank_version2/sideNavChecks.cs
@@ -31,13 +31,8 @@ namespace DemoBank_version2
                 IWebElement aboutLink = driver.FindElement(By.XPath("//*[@id=\"headerPanel\"]/ul[1]/li[2]/a"));
                 aboutLink.Click();
                 Thread.Sleep(TimeSpan.FromSeconds(2));
-
                 IWebElement aboutLinkAgain = driver.FindElement(By.XPath("//*[@id=\"headerPanel\"]/ul[1]/li[2]/a"));
                 aboutLinkAgain.Click(); 
-                //driver.Manage().Cookies.AddCookie(new Cookie("Test", "123"));
-                //var sessionID = driver.Manage().Cookies.GetCookieNamed("jsessionid").Value;
-                // Fail Issue 1: URL upon page change via nav link adds session ID to the URL.
-                // Cannot seem replicate this via manual checks. 
                 Assert.Equal("https://parabank.parasoft.com/parabank/about.htm", driver.Url);
             }
 
@@ -51,12 +46,15 @@ namespace DemoBank_version2
             options.AddArguments("start-maximized");
             using IWebDriver driver = new ChromeDriver(options);
             {
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
                 driver.Navigate().GoToUrl("https://parabank.parasoft.com/parabank/index.htm");
                 Assert.Equal("https://parabank.parasoft.com/parabank/index.htm", driver.Url);
 
                 IWebElement servicesLink = driver.FindElement(By.LinkText("Services"));
                 servicesLink.Click();
-                Thread.Sleep(3000);
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+                IWebElement servicesLinkAgain = driver.FindElement(By.LinkText("Services"));
+                servicesLinkAgain.Click();
                 Assert.Equal("https://parabank.parasoft.com/parabank/services.htm", driver.Url);
             }
 
